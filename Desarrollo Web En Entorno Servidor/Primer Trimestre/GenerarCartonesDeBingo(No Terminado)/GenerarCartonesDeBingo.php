01bcdb1682cb6a8c9b5bc49bcbbb235c7ff1d370
paurca <60507949+paurca@users.noreply.github.com>
2020-11-11 13:50:10 +0100

    Add files via upload

---
+++ Desarrollo Web En Entorno Servidor/Primer Trimestre/GenerarCartonesDeBingo(No Terminado)/GenerarCartonesDeBingo.php
@@ -0,0 +1,12 @@
+<?php
+    $col1 = range(1, 9);
+    $col2 = range(10, 19);
+    $col3 = range(20, 29);
+    $col4 = range(30, 39);
+    $col5 = range(40, 49);
+    $col6 = range(50, 59);
+    $col7 = range(60, 69);
+    $col8 = range(70, 79);
+    $col9 = range(80, 90);
+    if
+?>
